62af678a44428ebadb808d054c09108d27fa4739
Terry Fong <b03902122@ntu.edu.tw>
2015-03-30 22:45:02 +0800

    hard code app id and secret

--- config/initializers/02_omniauth.rb
+++ config/initializers/02_omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 #  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'user_about_me'}
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'user_friends,user_about_me'}
+# provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'user_friends,user_about_me'}
+  provider :facebook, '1430225577271122', '6a1659bda82e1ae0cb16705808e72ae1', {:scope => 'user_friends,user_about_me'}
 end
